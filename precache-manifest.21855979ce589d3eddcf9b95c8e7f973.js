88de8df0201a79c414a830c1a7df48563e606acc
Leon <FeelineKing@gmail.com>
2019-11-14 01:47:51 +0200

    Updates

---
+++ precache-manifest.21855979ce589d3eddcf9b95c8e7f973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf42679680931bf7ce769af3a5cc818",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "af3784c144b073cc2c47",
+    "url": "/react_add-todo-form/static/css/main.cdd8b7bf.chunk.css"
+  },
+  {
+    "revision": "3eb6f92a0c2c2fb8ce50",
+    "url": "/react_add-todo-form/static/js/2.240bb403.chunk.js"
+  },
+  {
+    "revision": "af3784c144b073cc2c47",
+    "url": "/react_add-todo-form/static/js/main.4ec0c08b.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
